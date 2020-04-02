2a880b3a58771d53e309d203a45b4dc4a72fdd54
Kai Tietz <ktietz@anaconda.com>
2020-04-02 17:54:38 +0200

    Map MAP_ANONYMOUS to MAP_ANON on osx

--- r-isospecr-feedstock/recipe/build.sh
+++ r-isospecr-feedstock/recipe/build.sh
@@ -10,6 +10,7 @@ else
   mv * $PREFIX/lib/R/library/IsoSpecR
 
   if [[ $target_platform == osx-64 ]]; then
+    export CXXFLAGS="${CXXFLAGS} -DMAP_ANONYMOUS=MAP_ANON "
     pushd $PREFIX
       for libdir in lib/R/lib lib/R/modules lib/R/library lib/R/bin/exec sysroot/usr/lib; do
         pushd $libdir || exit 1
