eb8ba85465c6450d99207aeeed6dba0a996f34b8
Build Machine <conda@anaconda.com>
2022-06-02 16:47:07 -0500

    Update to fix skeleton generated recipes.

--- r-bild-feedstock/recipe/build.sh
+++ r-bild-feedstock/recipe/build.sh
@@ -2,8 +2,9 @@
 
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
-  mv DESCRIPTION DESCRIPTION.old
-  grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+  # DESCRIPTION doesn't currently have Priority in it...
+  #mv DESCRIPTION DESCRIPTION.old
+  #grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
   $R CMD INSTALL --build .
 else
   mkdir -p $PREFIX/lib/R/library/bild
