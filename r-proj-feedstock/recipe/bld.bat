2d51f575e6d22caa8b9d0238cef303e50f511aa5
Serhii Kupriienko <skupriienko@anaconda.com>
2024-03-14 11:35:31 +0000

    Fixes for r-gmp, r-proj, r-proj4

--- r-proj-feedstock/recipe/bld.bat
+++ r-proj-feedstock/recipe/bld.bat
@@ -1,2 +1,7 @@
-"%R%" CMD INSTALL --build . %R_ARGS%
-IF %ERRORLEVEL% NEQ 0 exit /B 1
+set PKG_LIBS= -lproj -lsqlite3 -lcurl -lbcrypt -ltiff -ljpeg -lssl -lssh2 -lcrypto -lgdi32 -lz -lzstd -lwebp $(LIBSHARPYUV) -llzma -lgdi32 -lcrypt32 -liconv -lws2_32 -lwldap32 -lwinmm -lstdc++
+sed -i.bak "s/^PKG_LIBS/#PKG_LIBS/" src/Makevars.ucrt
+
+
+cat src/Makevars.ucrt
+
+"%R%" CMD INSTALL --build . --configure-args="%PKG_LIBS%"
