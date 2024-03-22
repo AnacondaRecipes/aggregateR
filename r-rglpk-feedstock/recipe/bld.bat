8b8f34fd7fcc4618f210c0883368c4145917391a
Serhii Kupriienko <skupriienko@anaconda.com>
2024-03-22 13:26:29 +0000

    Add new changes

--- r-rglpk-feedstock/recipe/bld.bat
+++ r-rglpk-feedstock/recipe/bld.bat
@@ -1,4 +1,5 @@
 sed -i.bak "s!^PKG_LIBS=-L${GLPK_HOME}/lib!PKG_LIBS=-L$(R_HOME)/../../Library/bin!" src/Makevars.win
+sed -i.bak "s!^PKG_LIBS=-L${GLPK_HOME}/lib!PKG_LIBS=-L$(R_HOME)/../../Library/bin!" src/Makevars.ucrt
 
 
 "%R%" CMD INSTALL --build .
