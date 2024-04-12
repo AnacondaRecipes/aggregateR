8d3ac72e48f8de87b7900a212ef122ee6af9a993
Serhii Kupriienko <skupriienko@anaconda.com>
2024-04-12 10:27:02 +0000

    Fix r-sdctable and r-leidenbase

--- r-leidenbase-feedstock/recipe/bld.bat
+++ r-leidenbase-feedstock/recipe/bld.bat
@@ -1,2 +1,6 @@
-"%R%" CMD INSTALL --build . %R_ARGS%
-IF %ERRORLEVEL% NEQ 0 exit /B 1
+set PKG_LIBS=-L$(R_HOME)/../../Library/bin -lxml2 -liconv -lz -lws2_32 -lstdc++ -lglpk -lgmp -lgfortran $(BLAS_LIBS) $(LAPACK_LIBS) -llzma
+
+sed -i.bak "s/^PKG_LIBS/#PKG_LIBS/" src/Makevars.ucrt
+
+"%R%" CMD INSTALL --build . --configure-args="%PKG_LIBS%"
+if errorlevel 1 exit 1
--- r-sdctable-feedstock/recipe/bld.bat
+++ r-sdctable-feedstock/recipe/bld.bat
@@ -1,2 +1,5 @@
-"%R%" CMD INSTALL --build . %R_ARGS%
-IF %ERRORLEVEL% NEQ 0 exit /B 1
+REM Fix the place of the GLPK library on Windows
+sed -i.bak "s!^PKG_LIBS=-L${GLPK_HOME}/lib!PKG_LIBS=-L$(R_HOME)/../../Library/bin!" src/Makevars.win
+
+"%R%" CMD INSTALL --build .
+IF %ERRORLEVEL% NEQ 0 exit 1
